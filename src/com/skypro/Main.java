d56a2f926b67370cd8fbd8baf32b61bbd05a1d86
Miklhail P <m.pojidaev@mail.ru>
2022-03-28 23:41:02 +0300

    выполнил дз №1

--- src/com/skypro/Main.java
+++ src/com/skypro/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    }
+        System.out.println("Домашнее задание");
+        System.out.println("Делаем коммит и пуш");
+        System.out.println("Переходим в гитхаб");
+
 }
